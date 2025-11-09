70eef3af048c4bc5adb68ea4ca7fb93e071c4888
TRKQA23 <tanmayqaa2023@gmail.com>
2025-11-09 10:40:12 +0530

    API Exception Class create, Enum Constant create

--- src/main/java/com/qa/apifm/Exceptions/APIExceptions.java
+++ src/main/java/com/qa/apifm/Exceptions/APIExceptions.java
@@ -2,4 +2,8 @@ package com.qa.apifm.Exceptions;
 
 public class APIExceptions extends RuntimeException {
 
+	public APIExceptions(String message) {
+		super(message);
+	}
+
 }
---
+++ src/main/java/com/qa/apifm/constant/AuthType.java
@@ -0,0 +1,12 @@
+package com.qa.apifm.constant;
+
+public enum AuthType {
+	
+	BEARER_TOKEN,
+    BASIC_AUTH,
+    API_KEY,
+    NO_AUTH
+
+
+
+}
